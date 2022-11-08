dac4a12e67e25fa59d3fdaf0bdb2b5332afb488c
Darkunitx <darkunitx@live.com>
2022-11-08 11:11:12 -0600

    added beginning prompts and alerts

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var passwordLength = parseInt(window.prompt("How many characters do you want in your password?"));
 
+  if (isNaN(passwordLength)) {
+    window.alert("You must enter a Valid Number.")
+    return null;
+
+  }
+
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("You must enter a number that is between 8 and 128")
+    window.alert("You must enter a number that is between 8 and 128")
+    
     return null;
     
   }
+
+  var isNumbers = confirm("Would you like Numbers in your password?");
+  var isUpperCase = confirm("Would you like Upper case letters in your password?");
+  var isLowerCase = confirm("Would you like Lower case letters in your password?");
+  var IsSpecialChar= confirm("Would you like Special Characters in your password?");
 
 }
 
