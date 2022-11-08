da7ac3fa7f1e649ce7b44883345f0548a972c706
Darkunitx <darkunitx@live.com>
2022-11-08 10:52:13 -0600

    added passwordLength function in js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = parseInt(window.prompt("How many characters do you want in your password?"));
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must enter a number that is between 8 and 128")
+    return null;
+    
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
