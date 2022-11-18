5f37d3b78f4b2822d05de01205bbe304af78ff13
Darkunitx <darkunitx@live.com>
2022-11-17 23:57:56 -0600

    added two if statements with two alerts in the generatePassword function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
+var arrayStorage = [];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
@@ -8,32 +10,29 @@ var specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","<",">"
 
 function writePassword() {
 
-  var userChoice = window.prompt("Hello and welcome to Random Password Generator, This program will generate a random password based on the criteria that you have selected. You will be prompted to choose at least one of the following: Numbers, Upper Case letters, Lower Case letters or Special characters. Do you wish to continue?");
-
-  if (userchoice === false) {
-       return null;
-  }
-
-  else {
-  var length = parseInt(window.prompt("How long do you want your password to be?"));
-    
-  }
-
-
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 
 function generatePassword () {
 
+var userChoice = window.alert("Hello and welcome to Random Password Generator, This program will generate a random password based on the criteria that you have selected. You will be prompted to choose at least one of the following: Numbers, Upper Case letters, Lower Case letters or Special characters. Do you wish to continue?");
 
+var passLength = parseInt(window.prompt("How long do you want your password to be?"));
+
+  if (isNaN(passLength)) {
+    window.alert("You need to enter a number")
+    return;
+  }
+
+  if (passLength < 8 || passLength > 128) {
+    window.alert("You must choose a number between 8 and 128")
+    return;
+  }
+
+  
 }
-generatePassword ();
+generatePassword();
 
-
-generateBtn.addEventListener("click", writePassword);
