5d96d6de5117356eee7b9945e47be163127e9962
Darkunitx <darkunitx@live.com>
2022-11-18 14:46:17 -0600

    added if statements to see wether or not the 4 confirms are true or not

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-var arrayStorage = [];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","<",">","/"];
+var arrayStorage = [];
 
 
 function writePassword() {
@@ -20,24 +20,36 @@ function generatePassword () {
 
   var userChoice = window.alert("Hello and welcome to Random Password Generator, This program will generate a random password based on the criteria that you have selected. You will be prompted to choose at least one of the following: Numbers, Upper Case letters, Lower Case letters or Special characters. Do you wish to continue?");
 
-  var passLength = parseInt(window.prompt("How long do you want your password to be?"));
+  var passwordLength = parseInt(window.prompt("How long do you want your password to be?"));
 
-  if (isNaN(passLength)) {
+  if (isNaN(passwordLength)) {
     window.alert("You need to enter a number")
     return;
   }
 
-  if (passLength < 8 || passLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     window.alert("You must choose a number between 8 and 128")
     return;
   }
 
-  var includingNumbers = window.prompt("Do you want Numbers in your password?");
-  var includingUpper = window.prompt("Do you want Uppercase Letters in your password?");
-  var includingLower = window.prompt("Do you want Lowercase Letters in your password?");
-  var includingSpecial = window.prompt("Do you want Special Characters in your password?");
+  var includingNumbers = window.confirm("Do you want Numbers in your password?");
+  var includingUpper = window.confirm("Do you want Uppercase Letters in your password?");
+  var includingLower = window.confirm("Do you want Lowercase Letters in your password?");
+  var includingSpecial = window.confirm("Do you want Special Characters in your password?");
 
+  if (includingNumbers) {
+    arrayStorage.push.apply(arrayStorage, numbers);
+  }
+  if (includingUpper) {
+    arrayStorage.push.apply(arrayStorage, upperCaseLetters);
+  }
+  if (includingLower) {
+    arrayStorage.push.apply(arrayStorage, lowerCaseLetters);
+  }
+  if (includingSpecial) {
+    arrayStorage.push.apply(arrayStorage, specialCharacters);
+  }
 
 }
-generatePassword();
 
+console.log(arrayStorage);
